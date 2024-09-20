5b76b0e92524c699df13f5f3938c01e30d655d83
Pankaj Tyagi <itstyagi0001@gmail.com>
2024-09-20 12:38:36 +0530

    Fetching the Data - Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,65 @@
 import { useEffect, useState } from "react";
 
 export default function App() {
-    const [counter, setCounter] = useState(0);
-    const [sync, setSync] = useState(false);
-    
-    useEffect(()=>{
-        console.log("Rendering...");
-        document.title="React Tutorial";
-    }, [sync]);
+  const [counter, setCounter] = useState(0);
+  const [sync, setSync] = useState(false);
 
-    function Incrementer() {
-        setCounter((prevCount) => prevCount +1 );
-    }
+  useEffect(() => {
+    console.log("Rendering...");
+    document.title = "React Tutorial";
+  }, [sync]);
 
-    function SyncCounter() {
-        setSync((current) => !current);
+  //   useEffect(() => {
+  //     fetch("https://jsonplaceholder.typicode.com/users", {
+  //       method: "GET",
+  //     })
+  //       .then((response) => {
+  //         return response.json();
+  //       })
+  //       .then((data) => {
+  //         console.log(data);
+  //       })
+  //       .catch((err) => {
+  //         console.log("Invalid Endpoint :",err);
+  //       });
+  //   });
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {
+        console.log("Some Error in Fetching :", error);
+      }
     }
-    return (
-        <div>
-            <div>You have Clicked the button {counter} times.</div>
-            <button onClick={Incrementer}>Click ME {counter}</button> &emsp;
-            <button onClick={SyncCounter}>Sync</button>
-        </div>
-    );
+    fetchUsers();
+  });
+
+  function Incrementer() {
+    setCounter((prevCount) => prevCount + 1);
+  }
+
+  function SyncCounter() {
+    setSync((current) => !current);
+  }
+  return (
+    <>
+      <h2>
+        Hola! This is our React Tutorial by
+        <br />
+        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&#8212;
+        Anson The Developer.
+      </h2>
+      <hr />
+      <div>
+        <div>You have Clicked the button {counter} times.</div>
+        <button onClick={Incrementer}>Click ME {counter}</button> &emsp;
+        <button onClick={SyncCounter}>Sync</button>
+      </div>
+    </>
+  );
 }
