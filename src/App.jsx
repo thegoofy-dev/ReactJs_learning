83ec192f6fdb6714ae3e90b78fe0020e68837c1e
Pankaj Tyagi <itstyagi0001@gmail.com>
2024-09-20 11:59:54 +0530

    useEffect Hook's Dependency Array

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import { useEffect, useState } from "react";
 
 export default function App() {
     const [counter, setCounter] = useState(0);
+    const [sync, setSync] = useState(false);
     
     useEffect(()=>{
         console.log("Rendering...");
-        document.title="React Tutorial " + counter;
-    })
+        document.title="React Tutorial";
+    }, [sync]);
 
     function Incrementer() {
         setCounter((prevCount) => prevCount +1 );
     }
+
+    function SyncCounter() {
+        setSync((current) => !current);
+    }
     return (
         <div>
-            <h3>You have Clicked the button {counter} times.</h3>
-            <button onClick={Incrementer}>Click ME {counter}</button>
+            <div>You have Clicked the button {counter} times.</div>
+            <button onClick={Incrementer}>Click ME {counter}</button> &emsp;
+            <button onClick={SyncCounter}>Sync</button>
         </div>
     );
 }
