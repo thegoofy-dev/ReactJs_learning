d3cf7d34f0b2f970bb2d4c5eadec10b27db98049
Pankaj Tyagi <itstyagi0001@gmail.com>
2024-09-20 11:44:57 +0530

    useEffect Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,20 @@
-import { useState } from "react";
-import { UserDetails } from "./Components/UserDeatils";
+import { useEffect, useState } from "react";
 
 export default function App() {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [counter, setCounter] = useState(4);
-    const [users, setUsers] = useState([
-        { id: 1, username: "anson", email: "anson@gmail.com" },
-        { id: 2, username: "john", email: "john@gmail.com" },
-        { id: 3, username: "jane", email: "jane@gmail.com" },
-    ]);
+    const [counter, setCounter] = useState(0);
+    
+    useEffect(()=>{
+        console.log("Rendering...");
+        document.title="React Tutorial " + counter;
+    })
 
-    const handleAddUser = (e) => {
-        e.preventDefault();
-        const newUser = { id: counter, username, email };
-        setCounter((prevCounter) => prevCounter + 1);
-        setUsers((prevUsers) => [...prevUsers, newUser]);
-        setUsername("");
-        setEmail("");
-    };
-
+    function Incrementer() {
+        setCounter((prevCount) => prevCount +1 );
+    }
     return (
-        <>
-            <h2>
-                Hola! This is our React Tutorial by
-                <br />
-                &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&#8212;
-                Anson The Developer.
-            </h2>
-            <hr />
-
-            <form onSubmit={handleAddUser}>
-                <h3 className="addUser">Add User</h3>
-                <div>
-                    <label htmlFor="username">UserName:</label>
-                    <input
-                        value={username}
-                        name="username"
-                        id="username"
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    &emsp;
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        value={email}
-                        name="email"
-                        id="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    &emsp;
-                    <button type="submit">Add User</button>
-                </div>
-            </form>
-            <hr />
-
-            <div>
-                {users.map((user) => (
-                    <UserDetails key={user.id} user={user} setUsers={setUsers} />
-                ))}
-            </div>
-        </>
+        <div>
+            <h3>You have Clicked the button {counter} times.</h3>
+            <button onClick={Incrementer}>Click ME {counter}</button>
+        </div>
     );
 }
