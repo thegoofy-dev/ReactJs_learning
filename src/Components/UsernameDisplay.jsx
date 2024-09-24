353205aa024a599bc7c1cc96f85625824e351e02
Pankaj Tyagi <itstyagi0001@gmail.com>
2024-09-24 19:21:33 +0530

    findByText Example

--- src/Components/UsernameDisplay.jsx
+++ src/Components/UsernameDisplay.jsx
@@ -1,7 +1,22 @@
-export function UsernameDisplay({username}) {
-    return (
-        <div>
-            <h2></h2>
-        </div>
-    )
+import { useEffect, useState } from "react" 
+import PropTypes from "prop-types";
+
+export function UsernameDisplay({ username }) {
+    const [timerPassed, setTimerPassed] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() =>{
+            setTimerPassed(true);
+        }, 2000);
+    })
+
+  return (
+    <div>
+      <span>{timerPassed && username}</span>
+    </div>
+  );
+}
+
+UsernameDisplay.propTypes = {
+    username: PropTypes.string.isRequired
 }
--- src/__tests__/UsernameDisplay.test.jsx
+++ src/__tests__/UsernameDisplay.test.jsx
@@ -2,15 +2,10 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { UsernameDisplay } from "../Components/UsernameDisplay";
 
-describe("UserName Display", () => {
-//   it("should render username", () => {
-//     render(<UsernameDisplay username={"Tillu"} />);
-//     expect(screen.getByText("Tillu")).toBeInTheDocument();
-//   }),
-  it("should not render username", () => {
+describe("UsernameDisplay", () => {
+  it("should render username", async () => {
     render(<UsernameDisplay username={"Tillu"} />);
-    // expect(screen.queryByText("Tillu")).toBeNull();
-    expect(screen.queryByText("Tillu")).not.toBeInTheDocument();
+    expect(await screen.findByText("Tillu", {}, { timeout: 8000})).toBeInTheDocument();
   })
 });
 
@@ -20,7 +15,7 @@ describe("UserName Display", () => {
 //      -> getBy... : Returns the matching node for a query, and throw a descriptive error if no elements match or if more than one match is found (use getAllBy instead if more than one element is expected).
 //      -> queryBy... : Returns the matching node for a query, and return null if no elements match. This is useful for asserting an element that is not present. Throws an error if more than one match is found (use queryAllBy instead if this is OK).
 //      -> findBy... : Returns a Promise which resolves when an element is found which matches the given query. The promise is rejected if no element is found or if more than one element is found after a default timeout of 1000ms. If you need to find more than one element, use findAllBy.
-   
+
 //     Multiple Elements
 //      -> getAllBy... : Returns an array of all matching nodes for a query, and throws an error if no elements match.
 //      -> queryAllBy... : Returns an array of all matching nodes for a query, and return an empty array ([]) if no elements match.
