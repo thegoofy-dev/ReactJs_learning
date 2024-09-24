0be27b34cb61c42e16142b7ebca4b5c7acfa25bf
Pankaj Tyagi <itstyagi0001@gmail.com>
2024-09-24 12:06:22 +0530

    Rendering Component Under Test

--- src/__tests__/UsernameDisplay.test.jsx
+++ src/__tests__/UsernameDisplay.test.jsx
@@ -1,7 +1,10 @@
-import { describe, it } from "vitest";
+import { describe, it } from "vitest"; 
+import { render } from "@testing-library/react";
+import { UsernameDisplay } from "../Components/UsernameDisplay";
 
 describe("UserName Display", () => {
     it("should render username", () => {
-        
+        render(<UsernameDisplay />);
+
     });
 })
